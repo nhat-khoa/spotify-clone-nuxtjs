9f3cd1eb883df51b46f6ace349dfd8f64ff4972b
khang0710 <117827755+khang0710@users.noreply.github.com>
2025-04-25 07:32:31 +0700

    profile update

--- components/Artist.vue
+++ components/Artist.vue
@@ -1,16 +1,24 @@
 <template>
   <NuxtLink to="/artist/1" class="mt-3 cover cover--round scale-animation">
-    <div class="cover__image">
-      <div class="ratio ratio-1x1">
-        <img src="/images/cover/large/5.jpg" alt="" />
-      </div>
+    <div class="ratio ratio-1x1" style="width: 220px">
+      <img
+        :src="artist.avatar_url || '/images/cover/large/5.jpg'"
+        alt=""
+        class="rounded-circle"
+      />
     </div>
     <div class="cover__foot">
-      <span class="cover__title text-truncate">Jina Moore</span>
+      <h6 class="cover__title text-truncate p-3">{{ artist.name }}</h6>
     </div>
   </NuxtLink>
 </template>
 
-<script setup></script>
-
+<script setup>
+defineProps({
+  artist: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
 <style></style>
---
+++ components/MyPlaylistCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-white border-0">
+    <img
+      :src="playlist.avatar_url || '/images/cover/small/1.jpg'"
+      class="card"
+      alt="Playlist image"
+      style="width: 200px"
+    />
+    <div class="card-body px-2 py-3">
+      <h5 class="card-title text-truncate mb-1">{{ playlist.name }}</h5>
+      <p class="card-text text-muted text-truncate mb-0">
+        {{
+          playlist.user?.full_name ||
+          playlist.user?.username ||
+          "Unknown Artist"
+        }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  playlist: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
